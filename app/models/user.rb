a2f6853bd339b3bc4adfb5f9ffe377fd07862497
Rodrigo Santanta Gonçalves <rodrigosg2000@gmail.com>
2018-05-22 14:08:27 +0000

    Translate status_enum for an user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
          :validatable
   enum status: [:active, :inactive]
   enum kind: [:admin, :secretary]
+
+  def status
+    status = read_attribute :status
+    I18n::t("activerecord.attributes.user.status_enum.#{status}")
+  end
+
 end
